c3f962c7f5af3b7df131b5ce6e8228f7d0a3fdce
Michael <admin@codeisle.com>
2013-09-03 07:27:34 -0400

    Added exception files (will eventually replace error enum).

---
+++ LibIPS.NET/Exceptions/Ips16MBException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeIsle.Exceptions
+{
+    class Ips16MBException
+    {
+    }
+}
