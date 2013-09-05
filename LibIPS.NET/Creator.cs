d8404c537c60a269e3d8d2657b3f0358b135b573
Michael <admin@codeisle.com>
2013-09-05 19:35:13 -0400

    Added classes to project.

---
+++ LibIPS.NET/Creator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeIsle
+{
+    class Creator
+    {
+        public void Create()
+        {
+        }
+    }
+}
