b17f9b98613c986fa7839fbffd6a25e0408a8e74
Michael <admin@codeisle.com>
2013-09-07 19:11:04 -0400

    Added constructors.

--- LibIPS.NET/Exceptions/Ips16MBException.cs
+++ LibIPS.NET/Exceptions/Ips16MBException.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace CodeIsle.LibIpsNet.Exceptions
 {
-    public class Ips16MBException: ApplicationException
+    [Serializable]
+    public class Ips16MBException : Exception
     {
+        public Ips16MBException()
+            : base() { }
 
+        public Ips16MBException(string message)
+            : base(message) { }
+
+        public Ips16MBException(string format, params object[] args)
+            : base(string.Format(format, args)) { }
+
+        public Ips16MBException(string message, Exception innerException)
+            : base(message, innerException) { }
+
+        public Ips16MBException(string format, Exception innerException, params object[] args)
+            : base(string.Format(format, args), innerException) { }
+
+        protected Ips16MBException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
     }
 }
--- LibIPS.NET/Exceptions/IpsScrambledException.cs
+++ LibIPS.NET/Exceptions/IpsScrambledException.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Runtime.Serialization;
 namespace CodeIsle.LibIpsNet.Exceptions
 {
-    public class IpsScrambledException: ApplicationException
+    [Serializable]
+    public class IpsScrambledException : Exception
     {
+        public IpsScrambledException()
+            : base() { }
+
+        public IpsScrambledException(string message)
+            : base(message) { }
+
+        public IpsScrambledException(string format, params object[] args)
+            : base(string.Format(format, args)) { }
+
+        public IpsScrambledException(string message, Exception innerException)
+            : base(message, innerException) { }
+
+        public IpsScrambledException(string format, Exception innerException, params object[] args)
+            : base(string.Format(format, args), innerException) { }
+
+        protected IpsScrambledException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
     }
 }
